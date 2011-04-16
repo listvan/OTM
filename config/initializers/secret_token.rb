1290a34b43a8d1dcc6e762966bf8cb2b54c902d7
Lakatos István <lakatos@bitandpixel.hu>
2011-04-16 15:43:48 +0200

    Initializes OTM project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otm::Application.config.secret_token = '19980f50cd9ad6dc314dc1d0c2accd5b4461786e87222b04015a8eb3cda8937bbdec6abbd3ebd375d88f01c165527218b0c93072bd42bd5d16b1fe4ca7179a4b'
