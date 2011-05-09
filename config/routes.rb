d3f190986587bbbd49c57515f3c596a60ebe69d4
Lakatos István <lakatos@bitandpixel.hu>
2011-05-10 00:28:16 +0200

    Adds routing for TasksController#create.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Otm::Application.routes.draw do
   get '/tasks' => 'tasks#index', :as => :tasks
   get '/tasks/:id' => 'tasks#show', :as => :task
+  post '/tasks' => 'tasks#create'
 end
--- spec/routing/tasks_routing_spec.rb
+++ spec/routing/tasks_routing_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe TasksController do
   describe "routing" do
-    it "recognizes /tasks" do
+    it "recognizes GET /tasks" do
       { :get => "tasks" }.should route_to(:controller => "tasks", :action => "index")
     end
 
@@ -10,12 +10,16 @@ describe TasksController do
       tasks_path().should == "/tasks"
     end
 
-    it "recognizes /tasks/1" do
+    it "recognizes GET /tasks/1" do
       { :get => "tasks/1" }.should route_to(:controller => "tasks", :action => "show", :id => "1")
     end
 
     it "generates /tasks/1" do
       task_path(1).should == "/tasks/1"
+    end
+
+    it "recognizes POST /tasks" do
+      { :post => "tasks" }.should route_to(:controller => "tasks", :action => "create")
     end
   end
 end
