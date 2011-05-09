ad452f781c460659c04b80c5d4efca56c8cf7f80
Lakatos István <lakatos@bitandpixel.hu>
2011-05-10 00:57:36 +0200

    Adds tests and implementations for Tasks#create.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,17 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     respond_with(@task)
   end
+
+  def create
+    @task = Task.new(params[:task])
+
+    Task.transaction do
+      @task.save!
+      flash[:notice] = t('models.task.created_successfully')
+      redirect_to(tasks_path)
+    end
+  rescue ActiveRecord::RecordInvalid
+    flash[:error] = t('models.task.create_failed')
+    render :action => :new, :status => :not_acceptable
+  end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -6,7 +6,7 @@ describe TasksController do
 
     it { should assign_to(:tasks) }
     it { should render_template('tasks/index') }
-    it { should respond_with(200) }
+    it { should respond_with(:success) }
   end
 
   describe "GET #show" do
@@ -15,6 +15,22 @@ describe TasksController do
 
     it { should assign_to(:task) }
     it { should render_template('tasks/show') }
-    it { should respond_with(200) }
+    it { should respond_with(:success) }
+  end
+
+  describe "POST #create with valid attributes" do
+    before(:each) { post :create, :task => { :title => "New Example Task", :priority => 5 } }
+
+    it { should assign_to(:task) }
+    it { should redirect_to(tasks_path) }
+    xit { should set_the_flash.to(:notice => 'Task has been successfully created!') }
+  end
+
+  describe "POST #create with invalid title" do
+    before(:each) { post :create, :task => { :title => "A", :priority => 5 } }
+
+    it { should assign_to(:task) }
+    it { should render_template('tasks/new') }
+    it { should respond_with(:not_acceptable) }
   end
 end
